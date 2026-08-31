b97a7b2b93d3f7e0c51b145cdcc9bcaf41875120
atrakchi-com <atrakchi.com@gmail.com>
2026-08-31 09:11:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    نستخدم التخزين المؤقت أولاً لأنها نادراً ما تتغيّر. هذا يمنح التطبيق قابلية العمل
    دون إنترنت مع تفادي مشكلة تلقّي نسخة قديمة عالقة في الذاكرة المؤقتة. */
 
-const CACHE_NAME = 'alfallah-cache-v1';
+const CACHE_NAME = 'alfallah-cache-v2';
 const CORE_ASSETS = [
   './alfallah.html',
   './manifest.json',
